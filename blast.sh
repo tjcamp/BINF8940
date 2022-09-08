6bc6f9bc962a171826eeb5c64515add34247c921
tjcamp <39649876+tjcamp@users.noreply.github.com>
2022-09-08 14:12:37 -0400

    Update blast.sh
    Changed output directory

--- blast.sh
+++ blast.sh
@@ -5,12 +5,12 @@
 #SBATCH --cpus-per-task=2		                            # Number of cores per task - match this to the num_threads used by BLAST
 #SBATCH --mem=40gb			                                # Total memory for job
 #SBATCH --time=2:00:00  		                            # Time limit hrs:min:sec
-#SBATCH --output=/work/binf8940/tc88074/log.%j			    # Location of standard output and error log files (replace cbergman with your myid)
+#SBATCH --output=/work/gene8940/tc88074/log.%j			    # Location of standard output and error log files (replace cbergman with your myid)
 #SBATCH --mail-user=tc88074@uga.edu                    # Where to send mail (replace cbergman with your myid)
 #SBATCH --mail-type=END,FAIL                            # Mail events (BEGIN, END, FAIL, ALL)
 
 #set output directory variable
-OUTDIR="/work/binf8940/tc88074/blast-test"             # replace cbergman in the following line with your myid
+OUTDIR="/work/gene8940/tc88074/blast-test"             # replace cbergman in the following line with your myid
 
 #if output directory doesn't exist, create it
 if [ ! -d $OUTDIR ]
