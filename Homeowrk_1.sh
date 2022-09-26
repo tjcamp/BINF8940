91b649599783acdf1095391bc2c031b75761ee70
tjcamp <39649876+tjcamp@users.noreply.github.com>
2022-09-26 18:02:17 -0400

    Homework1
    CDS count

---
+++ Homeowrk_1.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+#SBATCH --job-name=CDS-count		                        # Job name
+#SBATCH --partition=batch		                            # Partition (queue) name
+#SBATCH --ntasks=1			                                # Single task job
+#SBATCH --cpus-per-task=2		                            # Number of cores per task - match this to the num_threads used by BLAST
+#SBATCH --mem=40gb			                                # Total memory for job
+#SBATCH --time=2:00:00  		                            # Time limit hrs:min:sec
+#SBATCH --output=/work/gene8940/tc88074/log.%j			    # Location of standard output and error log files (replace cbergman with your myid)
+#SBATCH --mail-user=tc88074@uga.edu                   # Where to send mail (replace cbergman with your myid)
+#SBATCH --mail-type=END,FAIL                            # Mail events (BEGIN, END, FAIL, ALL)
+
+#set output directory variable
+OUTDIR="/work/gene8940/tc88074/homework_1"             # replace cbergman in the following line with your myid
+
+#if output directory doesn't exist, create it
+if [ ! -d $OUTDIR ]
+then
+    mkdir -p $OUTDIR
+fi
+
+
+#transfer e.coli str using curl
+curl -s ftp://ftp.ensemblgenomes.org/pub/bacteria/release-37/gff3/bacteria_0_collection/escherichia_coli_str_k_12_substr_mg1655/Escherichia_coli_str_k_12_substr_mg1655.ASM584v2.37.gff3.gz | gunzip -c > $OUTDIR/ecoli_MG1655.gff
+
+#count CDS
+grep --color "ID=CDS" ecoli_MG1655.gff | cut -f 3 | sort | uniq -c > results.txt
