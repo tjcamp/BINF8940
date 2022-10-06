760fcf231f20ae8c4ba75eee954c5c66a8fd3897
tjcamp <39649876+tjcamp@users.noreply.github.com>
2022-10-06 10:04:31 -0400

    updated faCount command with right file name
    $OUTDIR/ecoli_MG1655_cds.fna, $OUTDIR/ecoli_MG1655_noncds.fna

--- Homework_2.sh
+++ Homework_2.sh
@@ -32,10 +32,10 @@ module load ucsc/359
 #convert gff file to bed format
 convert2bed --input=gff < $OUTDIR/ecoli_MG1655.gff > $OUTDIR/ecoli_MG1655.bed
 grep "CDS" $OUTDIR/ecoli_MG1655.bed > $OUTDIR/ecoli_MG1655_cds.bed
-samtools faidx $OUTDIR/ecoli_MG1655.fna > $OUTDIR/ecoli_MG1655.fna.fai
+samtools faidx $OUTDIR/ecoli_MG1655.fna
 cut -f1,2 $OUTDIR/ecoli_MG1655.fna.fai > $OUTDIR/ecoli_MG1655.genome.txt
 bedtools complement -i $OUTDIR/ecoli_MG1655_cds.bed -g $OUTDIR/ecoli_MG1655.genome.txt > $OUTDIR/ecoli_MG1655_intergenic.bed
 bedtools getfasta -fi $OUTDIR/ecoli_MG1655.fna -bed $OUTDIR/ecoli_MG1655_cds.bed -fo $OUTDIR/ecoli_MG1655_cds.fna
 bedtools getfasta -fi $OUTDIR/ecoli_MG1655.fna -bed $OUTDIR/ecoli_MG1655_intergenic.bed -fo $OUTDIR/ecoli_MG1655_noncds.fna
-faCount $OUTDIR/ecoli_MG1655.cds.fna -summary > $OUTDIR/results_cds.txt
-faCount $OUTDIR/ecoli_MG1655.noncds.fna -summary > $OUTDIR/results_noncds.txt
+faCount $OUTDIR/ecoli_MG1655_cds.fna -summary > $OUTDIR/results_cds.txt
+faCount $OUTDIR/ecoli_MG1655_noncds.fna -summary > $OUTDIR/results_noncds.txt
