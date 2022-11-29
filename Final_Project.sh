85f07fced8bd0be4d4cf1870978cb89a36713dc0
tjcamp <39649876+tjcamp@users.noreply.github.com>
2022-11-29 08:31:15 -0500

    Updated ploidy to GRCh38 instead of 2
    Updated ploidy as here we are working with human genome GRCh38

--- Final_Project.sh
+++ Final_Project.sh
@@ -43,10 +43,10 @@ module load BWA/0.7.17-GCC-8.3.0
 module load SAMtools/1.10-GCC-8.3.0
 
 bwa index GCF_000001405.fna
+
 bwa mem -t 6 GCF_000001405.fna ERR4048410_1.fastq.gz ERR4048410_2.fastq.gz | samtools view - -O BAM | samtools sort --threads 6 > ERR4048410.sorted.bam
 samtools index -@ 6 ERR4048410.sorted.bam
 
-bwa index GCF_000001405.fna
 bwa mem -t 6 GCF_000001405.fna ERR4048411_1.fastq.gz ERR4048411_2.fastq.gz | samtools view - -O BAM | samtools sort --threads 6 > ERR4048411.sorted.bam
 samtools index -@ 6 ERR4048411.sorted.bam
 
@@ -54,13 +54,13 @@ samtools index -@ 6 ERR4048411.sorted.bam
 module load BCFtools/1.10.2-GCC-8.3.0
 
 bcftools mpileup -Oz --threads 6 --min-MQ 60 -f GCF_000001405.fna ERR4048410.sorted.bam > ERR4048410.sorted.mpileup.vcf.gz
-bcftools call -Oz -m -v --threads 6 --ploidy 2 ERR4048410.sorted.mpileup.vcf.gz > ERR4048410.sorted.mpileup.call.vcf.gz
+bcftools call -Oz -m -v --threads 6 --ploidy GRCh38 ERR4048410.sorted.mpileup.vcf.gz > ERR4048410.sorted.mpileup.call.vcf.gz
 bcftools filter -Oz -e 'QUAL<40 || DP<10' ERR4048410.sorted.mpileup.call.vcf.gz > ERR4048410.sorted.mpileup.call.filter.vcf.gz
 bcftools view -H -v snps ERR4048410.sorted.mpileup.call.filter.vcf.gz | wc -l > results.snps.txt
 bcftools view -H -v indels ERR4048410.sorted.mpileup.call.filter.vcf.gz | wc -l > results.indels.txt
 
 bcftools mpileup -Oz --threads 6 --min-MQ 60 -f GCF_000001405.fna ERR4048411.sorted.bam > ERR4048411.sorted.mpileup.vcf.gz
-bcftools call -Oz -m -v --threads 6 --ploidy 2 ERR4048411.sorted.mpileup.vcf.gz > ERR4048411.sorted.mpileup.call.vcf.gz
+bcftools call -Oz -m -v --threads 6 --ploidy GRCh38 ERR4048411.sorted.mpileup.vcf.gz > ERR4048411.sorted.mpileup.call.vcf.gz
 bcftools filter -Oz -e 'QUAL<40 || DP<10' ERR4048411.sorted.mpileup.call.vcf.gz > ERR4048411.sorted.mpileup.call.filter.vcf.gz
 bcftools view -H -v snps ERR4048411.sorted.mpileup.call.filter.vcf.gz | wc -l > results.snps.txt
 bcftools view -H -v indels ERR4048411.sorted.mpileup.call.filter.vcf.gz | wc -l > results.indels.txt
